d81abfbff5e418e584f08db564434a82c2520cc8
Antonio Jerkovic <antoniojerka@gmail.com>
2023-09-20 00:48:09 +0200

    test

--- backend/controllers/authorizeUser.js
+++ backend/controllers/authorizeUser.js
@@ -1,5 +1,6 @@
 module.exports.authorizeUser = (socket, next) => {
   if (socket.request.session.user) {
+    console.log('...socket.request.session.user[0]',...socket.request.session.user[0])
     socket.user = { ...socket.request.session.user[0] };
     next();
   } else {
