25ee7982ec13e251cd27a779a7f89cc356fc59d6
Antonio Jerkovic <antoniojerka@gmail.com>
2023-10-03 21:59:14 +0200

    test

--- backend/expressServer.js
+++ backend/expressServer.js
@@ -20,7 +20,7 @@ const {
 } = require("./controllers/serverController");
 const { authorizeUser } = require("./controllers/authorizeUser");
 
-const PORT = 4000;
+const PORT = process.env.BACKEND_DOMAIN == 'localhost' ? 4000 : 443;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
