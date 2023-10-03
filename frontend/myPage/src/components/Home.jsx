3208661b217eee27595d13fe81aad68375f9440d
Antonio Jerkovic <antoniojerka@gmail.com>
2023-10-03 23:27:44 +0200

    test

--- frontend/myPage/src/components/Home.jsx
+++ frontend/myPage/src/components/Home.jsx
@@ -4,32 +4,32 @@ import { useContextComp } from "./MyContext";
 import capitalize from "../functions/capitalize";
 const Home = () => {
   const { user, setToggleInput, toggleInput } = useContextComp();
-  const makeRequestWithUserGesture = () => {
-    if (
-      navigator.userAgent.match(
-        /SAMSUNG|Samsung|SGH-[I|N|T]|GT-[I|N]|SM-[A|N|P|T|Z]|SHV-E|SCH-[I|J|R|S]|SPH-L/i
-      )
-    ) {
-      let promise = document.requestStorageAccess();
+  // const makeRequestWithUserGesture = () => {
+  //   if (
+  //     navigator.userAgent.match(
+  //       /SAMSUNG|Samsung|SGH-[I|N|T]|GT-[I|N]|SM-[A|N|P|T|Z]|SHV-E|SCH-[I|J|R|S]|SPH-L/i
+  //     )
+  //   ) {
+  //     let promise = document.requestStorageAccess();
 
-      promise.then(
-        (hasAccess) => {
-          console.log("allowed", hasAccess);
-        },
-        (reason) => {
-          console.log("denied", reason);
-        }
-      );
-      // your code for Samsung Smartphones goes here...
-    }
-  };
+  //     promise.then(
+  //       (hasAccess) => {
+  //         console.log("allowed", hasAccess);
+  //       },
+  //       (reason) => {
+  //         console.log("denied", reason);
+  //       }
+  //     );
+  //     // your code for Samsung Smartphones goes here...
+  //   }
+  // };
   return (
     <div id="home" onClick={() => toggleInput && setToggleInput(false)}>
       <h3>Home</h3>
       <h1>Welcome {capitalize(user.name)}</h1>
-      <div id="cookie-consent">
+      {/* <div id="cookie-consent">
         <button onClick={()=>makeRequestWithUserGesture()}>Accept</button>
-      </div>
+      </div> */}
     </div>
   );
 };
