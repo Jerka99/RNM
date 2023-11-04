458bca63c629a4f0b8db8986f02cb2182cf15993
Antonio Jerkovic <antoniojerka@gmail.com>
2023-11-04 01:24:33 +0100

    added seen and test chat

--- frontend/myPage/src/components/Chat/FriendFromListInChat.jsx
+++ frontend/myPage/src/components/Chat/FriendFromListInChat.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import capitalize from "../../functions/capitalize"
-const FriendFromList = ({el, onlineUsers, messageOutOfRoom}) => {
+import { TbMessageCircle2 } from "react-icons/tb";
 
+const FriendFromList = ({el, onlineUsers, messageOutOfRoom}) => {
+  
   return (
     <>
     <p>{`${capitalize(el.name)} ${capitalize(el.secondname)}`}</p>
 
     {/* <p>{`${el.name} ${el.secondname} ${el.userId}`}</p> */}
-    {messageOutOfRoom > 0 && <div>{messageOutOfRoom}</div>}
+    {messageOutOfRoom > 0 && <div id='new-messages'><TbMessageCircle2/><div><small>{messageOutOfRoom}</small></div></div>}
     <div
       className={`friend ${onlineUsers != "offline" ? "online" : ""}`}
     ></div>
